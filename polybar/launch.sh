2fb87247011a4adaeef2a68760c08dbaac32c3c0
mauriciojacobo <jacobomauricio044@gmail.com>
2023-12-15 23:07:16 -0500

    I do not know if BSPWM will work

---
+++ polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar top &
+
+if [[ $(xrandr -q | grep 'HDMI-A-0 connected') ]]; then
+	polybar top_external &
+fi
